a344bc069cda6b9f4618e380f5f3592befbd0be0
Luis Abreu0 <luis_1k@hotmail.com>
2018-11-17 19:40:43 -0200

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -24,14 +24,14 @@ public class Board {
 	}
 
 	public Piece piece(int row, int column) {
-		if (!positionExists(row, column)){
+		if (!positionExists(row, column)) {
 			throw new BoardException("Position not on the Board");
 		}
 		return pieces[row][column];
 	}
 
 	public Piece piece(Position position) {
-		if (!positionExists(position)){
+		if (!positionExists(position)) {
 			throw new BoardException("Position not on the Board");
 		}
 		return pieces[position.getRow()][position.getColumn()];
@@ -43,6 +43,19 @@ public class Board {
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the Board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
 	}
 
 	private boolean positionExists(int row, int column) {
